574e5bc9bda04fdeb6d24d0eb288bfd64c055bd8
Luis Aguiar <luisaguiarescola@gmail.com>
2019-05-01 14:30:44 +0100

    Made a basic user interface

--- src/AppStore.java
+++ src/AppStore.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class AppStore {
-    public static void main(String [] args) {
-        System.out.println("Hello there!");
+    public static void main(String[] args) {
+        userInterface();
+    }
+
+    private static void userInterface(){
+        System.out.println("------------------");
+        System.out.println("|  AppStore ALP  |");
+        System.out.println("------------------");
+        System.out.println("Para fazer login, clique 1");
+        System.out.println("Não tem conta? Registe-se! Clique 2");
+
+        Scanner in = new Scanner(System.in);
+        int number = in.nextInt();
+
+        if(number == 1){
+            System.out.println("Username: ");
+            //Função para verificar se tal username existe
+            System.out.println("Palavra passe: ");
+            //Ver se o password coincide
+        }
+        else if(number == 2){
+            System.out.println("Insira um username: ");
+            //Função para guardar username
+            System.out.println("Insira uma palavra passe");
+            //Função para guardar password
+        }
+        else{
+            System.out.println("ERRO");
+        }
+
+        System.out.println("------------------");
     }
 }
